ae04e9158245c14f722968c5248b5c4066971d81
Joseph Allen Jemera <jajemera@hotmail.com>
2018-09-25 18:28:06 -0700

    Added pets and contact info to owners. Closes #33.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.bootstrap;
 
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -9,6 +10,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner
@@ -37,26 +40,65 @@ public class DataLoader implements CommandLineRunner
         Owner owner1 = new Owner();
         owner1.setFirstName("Jei");
         owner1.setLastName("Jemera");
+        owner1.setAddress("123 main st");
+        owner1.setCity("Sacramento");
+        owner1.setTelephone("9161231212");
+
+        Pet jeisPet = new Pet();
+        jeisPet.setPetType(savedDogPetType);
+        jeisPet.setBirthDate(LocalDate.now().minusYears(7));
+        jeisPet.setName("Eon");
+        owner1.getPets().add(jeisPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Grace");
         owner2.setLastName("Jemera");
+        owner1.setAddress("222 main st");
+        owner1.setCity("Sacramento");
+        owner1.setTelephone("3331231212");
+
+        Pet gracesPet = new Pet();
+        gracesPet.setPetType(savedDogPetType);
+        gracesPet.setBirthDate(LocalDate.now().minusYears(7));
+        gracesPet.setName("Simmy");
+        owner2.getPets().add(gracesPet);
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Serah");
         owner3.setLastName("Jemera");
+        owner1.setAddress("333 main st");
+        owner1.setCity("Sacramento");
+        owner1.setTelephone("2221231212");
+
+        Pet serahsPet = new Pet();
+        serahsPet.setPetType(savedCatPetType);
+        serahsPet.setBirthDate(LocalDate.now().minusYears(7));
+        serahsPet.setName("lol");
+        owner3.getPets().add(serahsPet);
 
         ownerService.save(owner3);
 
         Owner owner4 = new Owner();
         owner4.setFirstName("Matthew");
         owner4.setLastName("Jemera");
+        owner1.setAddress("444 main st");
+        owner1.setCity("Sacramento");
+        owner1.setTelephone("1111231212");
+
+        Pet matthewsPet = new Pet();
+        matthewsPet.setPetType(savedCatPetType);
+        matthewsPet.setBirthDate(LocalDate.now().minusYears(7));
+        matthewsPet.setName("robocar");
+        owner4.getPets().add(matthewsPet);
 
         ownerService.save(owner4);
+
+
+
 
         System.out.println("Loading owners...");
 
